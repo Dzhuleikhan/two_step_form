0e4f44fd24c618c838b0b5cf0d67c8b082089e41
dzhuleikhan <dzhuleikhan@gmail.com>
2025-01-23 15:21:14 +0300

    Dynamic bonus, favicon, page title

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -100,6 +100,30 @@ twoStepBonusCheckbox.forEach((checkbox) => {
   });
 });
 
+const settingInitialBonusValue = () => {
+  if (exceptCurrencies.includes(twoStepFormData.currency)) {
+    document.querySelector(".two-step-bonus-percent").innerHTML = "100%";
+    document.querySelector(".two-step-bonus-spins").innerHTML = "200FS";
+    document
+      .querySelector(".welcome-bonus-input")
+      .setAttribute("data-text", "100% on your first deposit");
+    document.querySelectorAll(".applied-bonus-percent").forEach((el) => {
+      el.innerHTML = "100%";
+    });
+  } else {
+    document.querySelector(".two-step-bonus-percent").innerHTML = "200%";
+    document.querySelector(".two-step-bonus-spins").innerHTML = "25FS";
+    document
+      .querySelector(".welcome-bonus-input")
+      .setAttribute("data-text", "200% on your first deposit");
+    document.querySelectorAll(".applied-bonus-percent").forEach((el) => {
+      el.innerHTML = "200%";
+    });
+  }
+};
+
+settingInitialBonusValue();
+
 // | INPUTS
 const twoStepGeneralInput = document.querySelectorAll(
   ".two-step-general-input",
