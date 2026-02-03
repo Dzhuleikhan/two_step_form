0cb1d1fdb77ee4912fb89abd460d1582713ff6b5
dzhuleikhan <dzhuleikhan@gmail.com>
2026-02-03 23:14:21 +0400

    Access-Control-Allow-Origin

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -207,6 +207,11 @@ if (twoStepPromocodeWrapper) {
     const fetchPromocodes = async () => {
       const res = await fetch(
         `https://api.goldbet.gg/api/v2/promocode/check-available?code=${promoCode.toUpperCase()}`,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        },
       );
       const data = await res.json();
       return data.available;
