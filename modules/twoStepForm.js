930996ccafa89071ef371831120df2e4de866315
dzhuleikhan <dzhuleikhan@gmail.com>
2026-02-03 23:11:32 +0400

    promocode api

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -206,7 +206,7 @@ if (twoStepPromocodeWrapper) {
 
     const fetchPromocodes = async () => {
       const res = await fetch(
-        `https://${newDomain}/api/v2/promocode/check-available?code=${promoCode.toUpperCase()}`,
+        `https://api.goldbet.gg/api/v2/promocode/check-available?code=${promoCode.toUpperCase()}`,
       );
       const data = await res.json();
       return data.available;
