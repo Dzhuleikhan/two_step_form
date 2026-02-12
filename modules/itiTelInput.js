31a20d33110dfd196e457dcc781122c7a117c4ec
dzhuleikhan <dzhuleikhan@gmail.com>
2026-02-12 14:04:41 +0400

    vps and inputmask

--- modules/itiTelInput.js
+++ modules/itiTelInput.js
@@ -1,5 +1,6 @@
 import intlTelInput from "intl-tel-input/intlTelInputWithUtils";
 import { geoData } from "./geoLocation";
+import Inputmask from "inputmask";
 
 const twoStepPhoneInput = document.querySelector(".two-step-phone-input");
 
@@ -17,4 +18,27 @@ export const twoStepiti = intlTelInput(twoStepPhoneInput, {
   useFullscreenPopup: false,
   autoPlaceholder: "polite",
   geoIpLookup,
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/16.0.0/js/utils.js",
+  customPlaceholder: function (selectedCountryPlaceholder) {
+    return selectedCountryPlaceholder.replace(/[0-9]/g, "X");
+  },
 });
+
+const applyMask = () => {
+  const placeholder = twoStepPhoneInput.getAttribute("placeholder");
+
+  if (!placeholder) return;
+
+  const maskPattern = placeholder.replace(/X/g, "9");
+
+  Inputmask({
+    mask: maskPattern,
+    placeholder: "X",
+    clearMaskOnLostFocus: true,
+  }).mask(twoStepPhoneInput);
+};
+
+twoStepPhoneInput.addEventListener("focus", applyMask);
+twoStepPhoneInput.addEventListener("click", applyMask);
+twoStepPhoneInput.addEventListener("countrychange", applyMask);
