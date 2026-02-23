6746c0fcf387a089253afd429e20f06c8ceeb571
dzhuleikhan <dzhuleikhan@gmail.com>
2026-02-23 17:03:23 +0400

    one tap auth

--- modules/twoStepForm.js
+++ modules/twoStepForm.js
@@ -969,3 +969,20 @@ twoStepFormMain.addEventListener("submit", (e) => {
 });
 
 gsap.to(".preloader", { opacity: 0, duration: 0.25, delay: 0.5 });
+
+// One-tap google auth
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id:
+      "757023558262-l0ffftmca719f5a4ksq4r5l2rugankkn.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+    auto_select: false,
+    cancel_on_tap_outside: true,
+  });
+
+  google.accounts.id.prompt();
+};
+
+function handleCredentialResponse() {
+  window.location.href = `https://${newDomain}/api/register?env=prod&type=google&currency=${twoStepFormData.currency}${twoStepFormData.promocode ? "&promocode=" + twoStepFormData.promocode : ""}&lang=${twoStepFormData.lang}${cid ? "&cid=" + cid : ""}${partner ? "&partner=" + partner : ""}${offer ? "&offer=" + offer : ""}`;
+}
