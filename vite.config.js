7cea4df723eee6ba588525dc0c0db11a5fe4e910
dzhuleikhan <dzhuleikhan@gmail.com>
2026-03-30 01:49:14 +0400

    base url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "https://landing-res.b-cdn.net/chicken-road/nodep/",
+  base: "https://landing-res.b-cdn.net/chicken-road/nodep/10/",
   esbuild: {
     supported: {
       "top-level-await": true,
